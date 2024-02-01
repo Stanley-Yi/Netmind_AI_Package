8fc960ab9f58e8b9d5e9854e10a545fc702344dd
Bin Liang <bin.liangmathematicsstudent@gmail.com>
2024-02-01 05:36:10 -0500

    add the first subpackage memory

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netmindxyz',
+    version='0.1',
+    author='BlackSheep Netmind.AI',
+    author_email='xiangpeng.wan@protagolabs.com',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'langchain',
+        'openpyxl',
+        'networkx',
+        'pymilvus',
+        'pymongo',
+        'pytest',
+    ],
+    # commandline-app 
+    # entry_points={
+    #     'console_scripts': [
+    #         'memory=memory_scripts.script:main',
+    #     ],
+    # },
+    description='A package which can help user to using memory tool to create and search memory',
+)
